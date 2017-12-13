3b4fa253d85c56c6c2411f339f5f31dc0fe445b8
Lindsay Brown <lindsaybrown@Lindsays-MacBook.local>
2017-12-13 17:38:17 -0500

    set up AJAX call to trivia API and displayQuestion function, additional styling added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,73 @@
 
 //set up an object containing trivia questions, corresponding correct answers, and wrong (filler) answers
-var trivia = {
-	questions = ["question 1", "question 2", "question 3", "question 4", "question 5", "question 6", "question 7", "question 8", "question 9", "question 10"],
-	correctAnswers = ["correct answer 1", "correct answer 2", "correct answer 3", "correct answer 4", "correct answer 5", "correct answer 6", "correct answer 7", "correct answer 8", "correct answer 9", "correct answer 10"],
-	fillerAnswers1 = ["filler 1", "filler 2", "filler 3", "filler 4", "filler 5", "filler 6", "filler 7", "filler 8", "filler 9", "filler 10"],
-	fillerAnswers2 = ["filler 11", "filler 12", "filler 13", "filler 14", "filler 15", "filler 16", "filler 17", "filler 18", "filler 19", "filler 20"]
-}
-
-//set variables for incorrect and correct answers that will increment conditionally
+var trivia;
+// //set variables for incorrect and correct answers that will increment conditionally
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-//set index variable to use for incrementing through question and answer arrays
+// //set index variable to use for incrementing through question and answer arrays
 var index = 0;
 
-//define a function to display the next question in the trivia.questions array and a countdown clock. will also be used to reset the game on button click.
-function displayQuestion()
-{
 
-}
 
-//define a function to display a screen when the correct answer is chosen, then calls the displayQuestion function after a setTimeout
-function correctAnswer()
-{
-
-}
-
-//define a function to display a screen when either the wrong answer is chosen or the player runs out of time, then calls the displayQuestion function after a setTimeout
-function incorrectAnswer()
-{
-
-}
-
+$.ajax({
+	url:"https://opentdb.com/api.php?amount=10&category=27&type=multiple",method: "GET"}).done(function(response){
+			
+		trivia = response;
+		
 //set conditional to display final screen after last question in the trivia.questions array is shown to display final score and reset game button
+		if (index >= trivia.results.length)
+		{
+			$(".display").html("Woo! Your score is: <br>Correct: " + correctAnswers + "<br>Incorrect: " + incorrectAnswers);
+			$(".display").append("<br><br><button class='button' id='replay'>Play Again</button>");
+		}
+		else{displayQuestion();}
 
+// //define a function to display the next question in the trivia.questions array and a countdown clock. will also be used to reset the game on button click.
+
+	  function displayQuestion()
+		{
+			var answers = [trivia.results[index].incorrect_answers[0], trivia.results[index].incorrect_answers[1], trivia.results[index].incorrect_answers[2], trivia.results[index].correct_answer];
+			
+			function shuffle (array) {
+	  		var i = 0
+	    		, j = 0
+	    		, temp = null
+
+	  		for (i = array.length - 1; i > 0; i -= 1) {
+	    		j = Math.floor(Math.random() * (i + 1))
+	    		temp = array[i]
+	    		array[i] = array[j]
+	    		array[j] = temp
+	  		}
+	  	}
+
+	  	shuffle(answers);
+			
+			$(".display").html(trivia.results[index].question + "<br>");
+			
+			for (k = 0; k < answers.length; k++){
+				$(".display").append("<button class='button'>" + answers[k] + "</button>");
+				}
+		}
+
+
+// //define a function to display a screen when the correct answer is chosen, then calls the displayQuestion function after a setTimeout
+// function correctAnswer()
+// {
+// 	index++;
+// }
+
+// //define a function to display a screen when either the wrong answer is chosen or the player runs out of time, then calls the displayQuestion function after a setTimeout
+// function incorrectAnswer()
+// {
+// 	index++;
+// }
+
+
+
+
+  
+
+
+
+});
